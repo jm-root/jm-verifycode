82d8a5543c7a1e814d0efb393557919701f1f04f
jamma <jeff@jamma.cn>
2018-08-14 14:22:02 +0800

    config 环境变量

---
+++ config/custom-environment-variables.js
@@ -0,0 +1,7 @@
+module.exports = {
+  redis: 'redis',
+  verifycode_length: 'verifycode_length',
+  verifycode_expire: 'verifycode_expire',
+  verifycode_key: 'verifycode_key',
+  secret: 'secret'
+}
